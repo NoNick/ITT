89880f2a2069117e0d1fd13b5c70b05c93bb0394
NoNick <aleksandrs634@gmail.com>
2015-10-28 19:56:50 +0300

    lecture 4

--- flyingleafe.v
+++ flyingleafe.v
@@ -311,15 +311,13 @@ Section Relation_definitions.
     Definition reflexive : Prop := forall a, R a a.
     Definition symmetric : Prop := forall a b, R a b -> R b a.
     Definition transitive : Prop := forall a b c, R a b -> R b c -> R a c.
-    Definition equiv : Prop := reflexive /.\ symmetric /.\ transitive.
+    Definition equiv : Prop := reflexive /\ symmetric /\ transitive.
   End General_properties.
   
   Section Meta_relations.
     Definition inclusion (R1 R2 : Relation A) : Prop := forall x y : A, R1 x y -> R2 x y.
     Definition equal_rels (R1 R2 : Relation A) : Prop :=
-      inclusion R1 R2 /.\ inclusion R2 R1.
-    Definition minimal_refl : Prop :=
-      forall (S : Relation A), reflexive -> inclusion R S. 
+      inclusion R1 R2 /\ inclusion R2 R1.
   End Meta_relations.
 End Relation_definitions.
 
@@ -331,22 +329,111 @@ Section Extensionality_definition.
   Definition extensional (f : A -> B) : Prop := forall x y, R1 x y -> R2 (f x) (f y).
 End Extensionality_definition.
 
+(* minimal_refl shouldn't be written as forall (S : Relation A), reflexive R -> inclusion R S.
+Section Absurd_min_refl.
+  Variable A : Type.
+  Variable x : A.
+  Variable R : Relation A.
+  Hypothesis reflR : reflexive A R.
+  Hypothesis minR: minimal_refl A R.
+
+  Theorem min_refl : False.
+  Proof.
+    pose (S (x y : A) := False).
+    unfold minimal_refl in minR.
+    pose (RS := minR S reflR).
+    unfold inclusion in RS.
+    unfold S in RS.
+    pose (notR := RS x x).
+    
+    absurd (R x x).
+    unfold not.
+      exact notR.
+      unfold reflexive in reflR.
+      exact (reflR x).
+  Qed.
+End Absurd_min_refl. *)
+
 Section Theorem_about_min_refl_relation.
   Variable A : Type.
   Variable R : Relation A.
   Hypothesis reflR : reflexive A R.
-  Hypothesis minR : minimal_refl A R.
+  (* minimal reflexive relation *)
+  Hypothesis minR : forall S : Relation A, reflexive A S -> inclusion A R S.
+
   Theorem min_reflex_rel_is_equiv_and_ext  :
-    (equiv A R) /.\ forall (B : Type) (R2 : Relation B), equiv B R2 -> forall f: A -> B, extensional A B R R2 f.
+    (equiv A R) /\ forall (B : Type) (R2 : Relation B), equiv B R2 -> forall f: A -> B, extensional A B R R2 f.
   Proof.
-    unfold equiv.
+    refine (conj _ _).
+      (* equiv *)
+      unfold equiv.
+      (* reflexive *)
+      refine (conj _ _).
+      exact reflR.
+      refine (conj _ _).
+        (* symmetric *)
+        unfold symmetric.
+        unfold inclusion in minR.
+        pose (S x y := R y x).
+        pose (R_symm := minR S reflR).
+        unfold S in R_symm.
+        exact R_symm.
+        (* transitive *)
+        unfold transitive.
+        pose (S x y := forall z, R y z -> R x z).
+        assert (reflS : reflexive A S).
+          unfold reflexive, S.
+          intuition.
+        pose (RS := minR S reflS).
+        unfold inclusion, S in RS.
+        intros a b c R_ab.
+        exact (RS a b R_ab c).
+        
+      (* ext *)
+      intros B R2 eq.
+      unfold extensional.
+      unfold inclusion in minR.
+      intros f x y.
+      pose (S x y := R2 (f x) (f y)).
+      assert (reflS : reflexive A S).
+        unfold reflexive.
+        unfold S.
+        unfold equiv in eq.
+        pose (reflR2 := proj1 eq).
+        unfold reflexive in reflR2.
+        intros a.
+        exact (reflR2 (f a)).
+      pose (RS := minR S reflS).
+      unfold S in RS.
+      exact (RS x y).
   Qed.
 End Theorem_about_min_refl_relation.
 
+Fixpoint eq_nat (n : nat) (m : nat) : Bool :=
+  match n with
+    O => match m with
+           O => true'
+         | S _ => false'
+         end
+  | S n' => match m with
+              O => false'
+            | S m' => eq_nat n m'
+            end
+  end.
 
-eq_nat : Nat -> Nat -> Bool.
-Theorem th2 : Id (Nat, x, y) <-> Is_true (eq_nat(x, y)).
+Theorem th2 : forall (x y : nat), x == y <-> Is_true (eq_nat x y).
 Proof.
+  intros x y.
+  unfold iff.
+  refine (conj _ _).
+    case x.
+      case y.
+      simpl.
+      intuition.
+
+      intros n.
+      simpl.
+      
   (* => *)
   intros x_y.
   rewrite x_y.
@@ -374,12 +461,14 @@ Qed.
 
 (* Аксиомы выбора *)
 (* (forall X: X != Empty) & (forall y in X: y != Empty) => Декартово произведение семейства x != Empty *)
-(* S_w, (w in W) - семейство => exists f: w -> Union[S, w in W] forall w in W: f(w) in S_w *)
+(* S_w, (w in W) - семейство => (exists f: w -> Union[S, w in W]): (forall w in W) f(w) in S_w *)
 
-Variable R := Relation S T. (* S T : Set *)
-(* аксиома выбора *)
-Theorem TT : (forall x in S) (exists y in T): R(x, y) -> (exists f: S -> T) (forall x in S) R(x, f(x)).
-Proof.
+Section TT.
+  Definition Relation' (S T : Set) := S -> T -> Prop.
+  Variable S T : Set.
+  Variable R : Relation' S T.
+  
+  (* аксиома выбора *)
   (* Построим ans: Pi(S, [x] Sigma (T, R(x))) -> Sigma(S -> T, [f] Pi(S, [x]R(x, f(x))))
      Рассмотрим z in Pi(S, [x] Sigma(T, R(x)))
      x in S => z(x) in Sigma(T, R(x))
@@ -388,17 +477,32 @@ Proof.
      Заметим, что fst(z(x)) = (\x. fst(z(x))) (x)
      \y. snd(z(y)) in Pi(S, [z]R(z, (\x. fst(z(x))) (z)))
      \x. fst(z(x)) in S -> T
-     pair (\x. fst(z(x)), \f. \y. snd(z(y))) in Sigma (f in (Pi x in A) T(x)) (Pi x in S) R(x, f(x)) *)
-Qed.
+     pair (\x. fst(z(x)), \f. \y. snd(z(y))) in Sigma (f in (Pi (x in A) T(x))) Pi (x in S) R(x, f(x)) *)
+  Theorem TT : forall (x : S), exists (y: T), R x y -> exists (f: S -> T), forall (x : S), R x (f x).
+  Proof.
+(*    intros x.
+    assert (ans : Pi S (fun x => (Sigma T (R x))) -> Sigma (S -> T) (fun f => Pi S (fun x => (R x (f x))))).
+      intros z.
+      pose (zx := apply' _ _ z x).
+      unfold apply' in zx.
+      pose (fst_zx := fst' _ _ zx).
+      pose (snd_zx := snd' _ _ zx).
+      pose (lambdaX := fun x => fst' _ _ (apply' _ _ z x)).
+      pose (tmp := snd' _ _ (apply' _ _ z x)).
+      unfold apply' in tmp.
+      pose (lambdaY := tmp).*)
+    admit.
+  Qed.
+End TT.
 
 (* Сетоид - пара из сета и отношения экв. на нем: (S, =_s) *)
 (* R(x, y) [x in A : Setoid, y in B : Setoid], если forall x, y in A, u, v, in B: (R(x, u) /\ x =_A y /\ u =_B v) -> R(y, v) *)
 (* ZF - аксиома выбора
-   A, B - сетоиды, R - экст. отн.
-   (forall x in A) (exists y in B) : R(x, y) -> (exists экстенц. f in A -> B) f (forall x) R(x, f(x)) *)
+   A, B - сетоиды, R - экст. отношение
+   (forall x in A) (exists y in B) : R(x, y) -> (exists экстенц. f in A -> B) (forall x) R(x, f(x)) *)
 (* Аксиома уникального выбора
-   A, B - сетоиды, R - экст. отн.
-   (forall x in A) (exists единственный y in B) : R(x, y) -> (exists экстенц. f in A -> B) f (forall x) R(x, f(x))
+   A, B - сетоиды, R - экст. отношение
+   (forall x in A) (exists единственный y in B) : R(x, y) -> (exists экстенц. f in A -> B) (forall x) R(x, f(x))
    Proof.
      Возьмем f in TT.
      Рассмотрим x in A: R(x, f(x)).
